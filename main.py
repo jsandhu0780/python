9868a7cbc68e93d77005bba016a21bd48a06fb1f
sandhunov <30528860-sandhunov@users.noreply.replit.com>
2023-12-16 20:36:50 +0000

    Band Name Generator
    This function generates he custom band name for the user.

---
+++ main.py
@@ -0,0 +1,7 @@
+print("Welcome to the Band Name Generator")
+
+city = input("The city you grew up in?\n")
+
+pet = input("The name of your pet?\n")
+
+print("Your band name could be " + city + " " + pet)
